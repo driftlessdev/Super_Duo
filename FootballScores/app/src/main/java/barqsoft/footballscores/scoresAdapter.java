994db997641adfb554f17f119170868e9679eaa7
uwflatlander <tschaab@gmail.com>
2015-10-17 18:24:50 -0500

    Add content description to the share button

--- FootballScores/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
+++ FootballScores/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
@@ -72,6 +72,7 @@ public class scoresAdapter extends CursorAdapter
             TextView league = (TextView) v.findViewById(R.id.league_textview);
             league.setText(Utilies.getLeague(cursor.getInt(COL_LEAGUE),context));
             Button share_button = (Button) v.findViewById(R.id.share_button);
+            share_button.setContentDescription(context.getString(R.string.cd_share_score,cursor.getString(COL_HOME),cursor.getString(COL_AWAY)));
             share_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v)
