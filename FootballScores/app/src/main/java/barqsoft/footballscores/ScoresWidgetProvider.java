b900eb56e5ebcf691a87d846ecdc3d31aa26fb8f
uwflatlander <tschaab@gmail.com>
2015-12-28 11:55:01 -0600

    Widget placeholder

---
+++ FootballScores/app/src/main/java/barqsoft/footballscores/ScoresWidgetProvider.java
@@ -0,0 +1,21 @@
+package barqsoft.footballscores;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Tim on 12/28/2015.
+ */
+public class ScoresWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+}
